b49a7cff7a5592bfbd18a0e1b0feacfb34c36c7d
Vasu Kulkarni <vasu@redhat.com>
2019-06-12 11:44:11 -0700

    Move all defaults from config yaml to defaults.py
    Signed-off-by: Vasu Kulkarni <vasu@redhat.com>

--- ocs/defaults.py
+++ ocs/defaults.py
@@ -1,10 +1,6 @@
 """
-Defaults module.
-This module is automatically loaded with variables defined in
-conf/ocsci/default_config.yaml in its DEFAULTS section.
-If the variable can be used in some config file sections from ocsci/config.py
-module, plese put your defaults rather to mentioned default_config.yaml file!
-See the documentation in conf/README.md file to understand this config file.
+Defaults module. All the defaults used by OSCCI framework should
+reside in this module.
 PYTEST_DONT_REWRITE - avoid pytest to rewrite, keep this msg here please!
 """
 import os
@@ -16,10 +12,6 @@ STORAGE_API_VERSION = 'storage.k8s.io/v1'
 ROOK_API_VERSION = 'ceph.rook.io/v1'
 OCP_API_VERSION = 'project.openshift.io/v1'
 OPENSHIFT_REST_CLIENT_API_VERSION = 'v1'
-
-# Those variables below are duplicate at the moment from default_config.yaml
-# and once we drop support for old runner we will remove those variables from
-# here and will have them defined only on one place.
 
 # Be aware that variables defined above and below are not used anywhere in the
 # config files and their sections when we rendering config!
